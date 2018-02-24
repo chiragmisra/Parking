94e4176a5be39d987881544768c59a9ac5ee742c
Chirag Misra <Chirag@MacBook-Pro.local>
2018-02-24 19:00:07 +0000

    initial skeleton components and routes

--- pqParking/src/app/app.module.ts
+++ pqParking/src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ParkingspacesformComponent } from './parkingspacesform/parkingspacesform.component';
+import { ParkinglistdisplayComponent } from './parkinglistdisplay/parkinglistdisplay.component';
 
+const appRoutes: Routes = [
+  { path: 'addparkingspaces', component: ParkingspacesformComponent },
+  { path: '**', component: ParkinglistdisplayComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParkingspacesformComponent,
+    ParkinglistdisplayComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
---
+++ pqParking/src/app/parkinglistdisplay/parkinglistdisplay.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parkinglistdisplay',
+  templateUrl: './parkinglistdisplay.component.html',
+  styleUrls: ['./parkinglistdisplay.component.scss']
+})
+export class ParkinglistdisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
