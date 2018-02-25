bafc4312d79dce02e0def78aa67ea6ef4df8b6cd
Chirag Misra <Chirag@MacBook-Pro.local>
2018-02-25 22:04:09 +0000

    basic form validation with model

---
+++ pqParking/src/app/parkingspacesform/parkingSpaceModel.ts
@@ -0,0 +1,10 @@
+export class ParkingSpaceModel {  
+
+    constructor(
+        public monday: [string],
+        public tuesday: string,
+        public wednesday: string,
+        public thursday: string,
+        public friday: string
+    ){}
+}
